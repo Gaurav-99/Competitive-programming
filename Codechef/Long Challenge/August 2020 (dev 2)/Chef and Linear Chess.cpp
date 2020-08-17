cacafc2518ccdcc68d1f61458ed0a66ea03075d7
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-08-17 13:14:02 +0530

    Create Chef and Linear Chess.cpp

---
+++ Codechef/Long Challenge/August 2020 (dev 2)/Chef and Linear Chess.cpp
@@ -0,0 +1,45 @@
+//Keep calm and carry on..
+
+/*  Problem : Chef and Linear Chess
+    URL : https://www.codechef.com/AUG20B/problems/LINCHESS
+
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+
+# define fast           ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+# define pb             push_back
+# define pi             3.1415926535897932384626
+# define mod            1000000007
+# define endl          '\n'
+# define DEBUG          false
+# define F              first
+# define S              second
+# define loop1(i,a,b)   for(int i = a; i<b; i++)
+# define loop2(i,a,b)   for(int i = b-1; i>=a; i--)
+# pragma GCC optimize "trapv"
+
+ int main(){
+     fast;
+     int t;     cin>>t;
+
+     while(t--){
+        int n,k;    cin>>n>>k;
+        int p;
+        int ans = -1;
+
+        while (n--){
+            cin>>p;
+            if (p < k && k%p == 0)
+                ans = (ans <= p)? p: ans;
+        }
+         cout<<ans<<endl;
+     }
+
+  return 0;
+}
