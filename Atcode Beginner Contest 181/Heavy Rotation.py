8daf3ec94618228f571019bd565bc7b66cac5527
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-11-09 08:36:33 +0530

    Create Heavy Rotation.py

---
+++ Atcode Beginner Contest 181/Heavy Rotation.py
@@ -0,0 +1,13 @@
+#Keep calm and carry on..
+
+'''  Problem :Heavy Rotation
+    URL :https://atcoder.jp/contests/abc181/tasks/abc181_a
+'''
+n = int(input())
+
+if n % 2 == 0:
+    print("White")
+else:
+    print("Black")
+
+
