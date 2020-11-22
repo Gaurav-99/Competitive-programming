5d16864debc5cad8a8a93f416d75635f6381306d
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-11-22 19:58:52 +0530

    Create Determinant.py

---
+++ Atcoder/Practice/Determinant.py
@@ -0,0 +1,12 @@
+#Keep calm and carry on..
+
+'''  Problem : A - Determinant
+    URL : https://atcoder.jp/contests/abc184/tasks/abc184_a
+'''
+#def solve():
+
+if __name__ == '__main__':
+   a, b = map(int, input().split())
+   c, d = map(int, input().split())
+   ans = a*d - b*c
+   print(ans)
