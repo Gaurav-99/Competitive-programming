0e0d9a4e66be6c775722fc789d0e5468869cffbd
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-11-30 13:27:39 +0530

    Create Text Wrap.py

---
+++ HackerRank/Practice/Text Wrap.py
@@ -0,0 +1,17 @@
+#Keep calm and carry on..
+
+'''  Problem : 
+    URL : https://www.hackerrank.com/challenges/text-wrap/problem
+'''
+
+import textwrap
+
+def wrap(string, max_width):
+    wrapper = textwrap.TextWrapper(width= max_width) 
+    word_list = wrapper.fill(text=string) 
+    return word_list
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
