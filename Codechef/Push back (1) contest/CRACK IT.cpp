63211ba686cf082fafefdc41fbf76fd855ce6520
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-12-17 19:27:40 +0530

    Create CRACK IT.cpp

---
+++ Codechef/Push back (1) contest/CRACK IT.cpp
@@ -0,0 +1,61 @@
+
+//Keep calm and carry on..
+
+/*  Problem :CRACK IT
+    URL :https://www.codechef.com/PBK12020/problems/ITGUY17
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+
+
+# define fast           ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+# define pb             push_back
+# define pi             3.1415926535897932384626
+# define mod            1000000007
+# define endl          '\n'
+# define DEBUG          false
+# define F              first
+# define S              second
+# define loop1(i,a,b)   for(ll i = a; i<b; i++)
+# define loop2(i,a,b)   for(ll i = b-1; i>=a; i--)
+# pragma GCC optimize "trapv"
+
+int sumDigits(int no) {
+	return no == 0 ? 0 : no % 10 + sumDigits(no / 10) ;
+}
+
+void just_solved_it() {
+	ll n , k;	cin >> n >> k;
+	ll ans = 0;
+	if (n == 1)	ans = k;
+	else if (n % 2 == 0 && n > 1)	ans = ( n / 2 ) * k;
+	else ans = ((n + 1) / 2 ) * k;
+
+	cout << ans ;
+
+
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	fast;
+	ll t = 1;	cin >> t;
+
+	while (t--) {
+
+		just_solved_it();
+		cout << endl;
+	}
+
+	return 0;
+}
