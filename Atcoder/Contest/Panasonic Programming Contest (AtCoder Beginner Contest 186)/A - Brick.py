bc319c7d715e04df200da2ace8c506f7b361e611
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-12-19 19:40:12 +0530

    Create A - Brick.py

---
+++ Atcoder/Contest/Panasonic Programming Contest (AtCoder Beginner Contest 186)/A - Brick.py
@@ -0,0 +1,12 @@
+#Keep calm and carry on..
+
+'''  Problem : A - Brick
+    URL : https://atcoder.jp/contests/abc186/tasks/abc186_a
+'''
+#def solve():
+
+if __name__ == '__main__':
+
+	n , w = map(int,input().split())
+	print(n//w)
+        
