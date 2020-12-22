f87067396616cc57252ad54b4f9cb2e64669246d
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-12-23 00:37:03 +0530

    Create Subtraction.py

---
+++ Codechef/Other Challenges/Code Starter Challenge 2020/Subtraction.py
@@ -0,0 +1,16 @@
+#Keep calm and carry on..
+
+'''  Problem : Subtraction
+    URL : https://www.codechef.com/CSTR2020/problems/SUBTRACT
+'''
+#def solve():
+
+if __name__ == '__main__':
+	n, k = map(int, input().split())
+	for i in range(k):
+		if n % 10 == 0:
+			n //= 10
+		else:
+			n -= 1
+			print(n)	
+
