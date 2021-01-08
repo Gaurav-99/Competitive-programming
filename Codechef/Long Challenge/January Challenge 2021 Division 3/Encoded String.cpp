6990494485002bcce8c953959ea3d4bceeb8261b
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-01-09 00:10:51 +0530

    Update Encoded String.cpp

--- Codechef/Long Challenge/January Challenge 2021 Division 3/Encoded String.cpp
+++ Codechef/Long Challenge/January Challenge 2021 Division 3/Encoded String.cpp
@@ -1,1 +1,71 @@
 
+//Keep calm and carry on..
+
+/*  Problem :
+    URL :
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+
+
+# define fast           ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+# define pb             push_back
+# define pi             3.1415926535897932384626
+# define mod            1000000007
+# define endl          '\n'
+# define DEBUG          false
+# define F              first
+# define S              second
+# define loop1(i,a,b)   for(ll i = a; i<b; i++)
+# define loop2(i,a,b)   for(ll i = b-1; i>=a; i--)
+# pragma GCC optimize "trapv"
+
+int countDigit(ll n) {
+	return floor(log10(n) + 1);
+}
+int sumDigits(int no) {
+	return no == 0 ? 0 : no % 10 + sumDigits(no / 10) ;
+}
+
+void just_solved_it() {
+	ll n;	cin >> n;
+	string str, ans;
+	cin >> str;
+	int add = 0;
+
+	while (n) {
+
+		string bn = str.substr(add, 4);
+		int ch = stoi(bn, 0, 2) + 97;
+		ans += (char)ch;
+		add += 4;
+		n -= 4;
+	}
+	cout << ans;
+
+}
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	fast;
+	ll t = 1;	cin >> t;
+
+	while (t--) {
+
+		just_solved_it();
+		cout << endl;
+	}
+
+	return 0;
+}
