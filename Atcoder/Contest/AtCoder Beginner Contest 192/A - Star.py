9eb22e075660ee1993909c424fc40951edbe94ef
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-02-20 21:43:26 +0530

    Create A - Star.py

---
+++ Atcoder/Contest/AtCoder Beginner Contest 192/A - Star.py
@@ -0,0 +1,9 @@
+#Keep calm and carry on..
+
+'''  Problem : A - Star
+    URL : https://atcoder.jp/contests/abc192/tasks/abc192_a
+'''
+
+if __name__ == '__main__':
+	n = int(input())
+	print(100 - (n % 100))
