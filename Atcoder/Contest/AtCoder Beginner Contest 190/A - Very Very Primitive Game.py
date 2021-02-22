b976cf64b396ae891fa74a0ae3709cb4e231b180
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-02-22 18:37:24 +0530

    Update A - Very Very Primitive Game.py

--- Atcoder/Contest/AtCoder Beginner Contest 190/A - Very Very Primitive Game.py
+++ Atcoder/Contest/AtCoder Beginner Contest 190/A - Very Very Primitive Game.py
@@ -1,19 +1,13 @@
 #Keep calm and carry on..
 
-'''  Problem : A - Very Very Primitive Game
-    URL : https://atcoder.jp/contests/abc190/tasks/abc190_a
+'''  Problem : B - uNrEaDaBlE sTrInG
+    URL : https://atcoder.jp/contests/abc192/tasks/abc192_b
 '''
 
-if __name__ == '__main__':
-	a,b,c = map(int, input().split())
-
-	if c == 0:
-		if a > b:
-			print("Takahashi")
-		else :
-			print("Akoi")
-	else :
-		if b > a:
-			print("Akoi")
-		else :
-			pritn("Takahashi")
+a, b, c = map(int, input().split())
+if a < b:
+    print('Aoki')
+elif a > b:
+    print('Takahashi')
+else:
+    print('Aoki' if c == 0 else 'Takahashi')
